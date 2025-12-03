eda12f3dfa1c635315082f7a5e8ee03a75cfd0b1
nghlong3004 <nghlong3004@gmail.com>
2025-12-03 16:55:22 +0700

    feature: Initial Project

---
+++ src/main/java/com/vn/nghlong3004/client/BoomOnlineClient.java
@@ -0,0 +1,13 @@
+package com.vn.nghlong3004.client;
+
+/**
+ * Project: boom-online-client
+ *
+ * @author nghlong3004
+ * @since 12/3/2025
+ */
+public class BoomOnlineClient {
+  public static void main(String[] args){
+    System.out.println("Hello world!");
+  }
+}
